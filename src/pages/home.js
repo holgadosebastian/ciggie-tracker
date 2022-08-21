20622b367fd8f0def2c37875130144a24002e25a
holgadosebastian <holgado.sebastian@gmail.com>
2022-08-21 22:02:20 +0200

    Updates tracker and add gh-pages deployment script

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,11 @@
 import React, { Fragment, useState, useEffect } from 'react'
+import cn from 'classnames'
 
-import { updatePuchits, getTimeString } from '../utils/utils'
+import { updatePuchits, getDailyPuchits, getTimeString } from '../utils/utils'
 
 const Home = () => {
   const [puchits, setPuchits] = useState([])
+  const [todayPuchits, setTodayPuchits] = useState([])
   const todayDate = (new Date()).toLocaleDateString()
 
   useEffect(() => {
@@ -11,6 +13,10 @@ const Home = () => {
     console.log('savedPuchits', savedPuchits)
     setPuchits(savedPuchits)
   }, [])
+
+  useEffect(() => {
+    setTodayPuchits(getDailyPuchits(puchits))
+  }, [puchits])
 
   const onAddPuchit = () => {
     const newPuchit = {
@@ -22,22 +28,21 @@ const Home = () => {
     updatePuchits(updatedPuchits)
   }
 
-  console.log('puchits', puchits)
-
   return (
     <Fragment>
       <div className="flex flex-col justify-center items-center" style={{ height: '50vh' }}>
         <p className="mb-2">{todayDate}</p>
         <div className="rounded-full border-4 border-solid border-white w-32 h-32 flex justify-center items-center">
-          <span className="text-5xl">{puchits.length}</span>
+          <span className="text-5xl">{todayPuchits.length}</span>
         </div>
         <button className="bg-white text-violet-700 px-6 py-2 mt-4 rounded" onClick={onAddPuchit}>ADD</button>
       </div>
-      {puchits.length > 0 && (
+      {todayPuchits.length > 0 && (
         <div className="p-4 overflow-y-auto" style={{ height: '50vh' }}>
           <ul>
-            {puchits.map(({date}, index) => {
-              return <li key={index} className="px-4 py-2 mt-1 bg-violet-900">{getTimeString(date)}</li>
+            {todayPuchits.map(({date}, index) => {
+              console.log('index', index)
+              return <li key={index} className={cn('px-4', 'py-2', 'bg-violet-900', { 'mt-1': index > 0 } )}>{getTimeString(date)}</li>
             })}
           </ul>
         </div>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,10 @@
-export const updatePuchits = (puchits) => {
+export const updatePuchits = puchits => {
   localStorage.setItem('puchits', JSON.stringify(puchits))
+}
+
+export const getDailyPuchits = puchits => {
+  const todayDate = new Date()
+  return puchits.filter(({date}) => todayDate.toDateString() === (new Date(date)).toDateString())
 }
 
 export const getTimeString = date => {
