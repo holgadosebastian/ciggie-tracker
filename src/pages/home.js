24e2db5530862e75dd0b82b12f9e974322c4b287
Sebastian Holgado <holgado.sebastian@gmail.com>
2023-12-18 17:47:52 +0100

    Refactor Item component in Home

--- src/pages/home.js
+++ src/pages/home.js
@@ -34,56 +34,70 @@ const Home = ({
       </div>
       {todayPuchits.length > 0 && (
         <div className='h-3/6 overflow-y-auto p-4'>
-          <ul>
-            {todayPuchits.map(({ id, date }, index) => {
-              return (
-                <li
-                  onClick={() =>
-                    activePuchitId !== id
-                      ? onSetActivePuchit(id)
-                      : onSetActivePuchit(null)
-                  }
-                  key={id}
-                  className={cn(
-                    'pl-4',
-                    'pr-2',
-                    'h-8',
-                    'transition-colors',
-                    'flex',
-                    'justify-between',
-                    'items-center',
-                    {
-                      'mt-1': index > 0,
-                      'bg-violet-900': activePuchitId !== id,
-                      'bg-white': activePuchitId === id,
-                      'text-violet-900': activePuchitId === id
-                    }
-                  )}
-                >
-                  <p>{getTimeString(date)}</p>
-                  {activePuchitId === id && (
-                    <div className='flex'>
-                      <div
-                        className='h-8 w-8 text-center leading-8'
-                        onClick={() => onRemovePuchit(id)}
-                      >
-                        <Icon name='trash-alt' />
-                      </div>
-                      <div
-                        className='h-8 w-8 text-center leading-8'
-                        onClick={() => onSetActivePuchit(null)}
-                      >
-                        <Icon name='close' />
-                      </div>
-                    </div>
-                  )}
-                </li>
-              );
-            })}
+          <ul className='flex flex-col gap-2'>
+            {todayPuchits.map(({ id, date }) => (
+              <Item
+                id={id}
+                key={id}
+                date={date}
+                activePuchitId={activePuchitId}
+                onRemovePuchit={onRemovePuchit}
+                onSetActivePuchit={onSetActivePuchit}
+              />
+            ))}
           </ul>
         </div>
       )}
     </div>
+  );
+};
+
+const Item = ({
+  id,
+  date,
+  activePuchitId,
+  onSetActivePuchit,
+  onRemovePuchit
+}) => {
+  return (
+    <li
+      onClick={() =>
+        activePuchitId !== id ? onSetActivePuchit(id) : onSetActivePuchit(null)
+      }
+      key={id}
+      className={cn(
+        'pl-4',
+        'pr-2',
+        'h-8',
+        'transition-colors',
+        'flex',
+        'justify-between',
+        'items-center',
+        {
+          'bg-violet-900': activePuchitId !== id,
+          'bg-white': activePuchitId === id,
+          'text-violet-900': activePuchitId === id
+        }
+      )}
+    >
+      <p>{getTimeString(date)}</p>
+      {activePuchitId === id && (
+        <div className='flex'>
+          <div
+            className='h-8 w-8 text-center leading-8'
+            onClick={() => onRemovePuchit(id)}
+          >
+            <Icon name='trash-alt' />
+          </div>
+          <div
+            className='h-8 w-8 text-center leading-8'
+            onClick={() => onSetActivePuchit(null)}
+          >
+            <Icon name='close' />
+          </div>
+        </div>
+      )}
+    </li>
   );
 };
 
