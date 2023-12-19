7885ee01370702813408738aee9700819807015d
Sebastian Holgado <holgado.sebastian@gmail.com>
2023-12-19 15:58:35 +0100

    Update file names

---
+++ src/layout/Navbar.js
@@ -0,0 +1,99 @@
+import React, { useState, useContext } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import cn from 'classnames';
+
+import { Icon } from '../components';
+import MainContext from '../context/MainContext';
+
+const Navbar = () => {
+  const { pathname } = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <>
+      <header className='fixed inset-x-0 top-0 py-2 px-4 flex gap-x-3 justify-between bg-violet-700'>
+        <button onClick={() => setMenuOpen(!menuOpen)}>
+          <Icon name='navicon' />
+        </button>
+        <nav className='flex gap-4'>
+          <Link
+            to='/'
+            className={cn('uppercase', { underline: pathname === '/' })}
+          >
+            Home
+          </Link>
+          <Link
+            to='/history'
+            className={cn('uppercase', { underline: pathname === '/history' })}
+          >
+            History
+          </Link>
+        </nav>
+      </header>
+      <MenuDrawer isOpen={menuOpen} onMenuClose={() => setMenuOpen(false)} />
+    </>
+  );
+};
+
+const MenuDrawer = ({ isOpen, onMenuClose }) => {
+  return (
+    <div
+      className={cn(
+        'fixed',
+        'top-0',
+        'left-0',
+        'bg-violet-700',
+        'w-full',
+        'h-full',
+        'transition-all',
+        {
+          'opacity-0 pointer-events-none': !isOpen,
+          'opacity-1': isOpen
+        }
+      )}
+    >
+      <button
+        className='absolute top-2 right-2 w-10 h-10'
+        onClick={() => onMenuClose()}
+      >
+        <Icon name='close' />
+      </button>
+      <div className='max-w-xs mx-auto py-10'>
+        <ul className='flex w-full'>
+          <li>
+            <GoalUpdate />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const GoalUpdate = () => {
+  const { goal, setGoal } = useContext(MainContext);
+  const [updatedGoal, setUpdatedGoal] = useState(goal || 0);
+
+  return (
+    <div>
+      <p className='mb-2'>Set your daily goal</p>
+      <div className='flex h-10 w-full'>
+        <input
+          className='bg-transparent border-white border rounded rounded-r-none px-4'
+          type='number'
+          min='0'
+          max='100'
+          onChange={(e) => setUpdatedGoal(parseInt(e.target.value))}
+          value={updatedGoal}
+        />
+        <button
+          className='bg-white text-violet-700 px-4 uppercase rounded rounded-l-none'
+          onClick={() => setGoal(updatedGoal)}
+        >
+          Update
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect, useContext } from 'react';
+import cn from 'classnames';
+
+import { Icon } from '../components';
+import { getDailyPuchits, getTimeString } from '../utils/utils';
+import MainContext from '../context/MainContext';
+
+const Home = ({ todayDate }) => {
+  const [todayPuchits, setTodayPuchits] = useState([]);
+  const { goal, cigarettes, addCigarette } = useContext(MainContext);
+
+  useEffect(() => {
+    setTodayPuchits(getDailyPuchits(cigarettes).reverse());
+  }, [cigarettes]);
+
+  return (
+    <div className='h-screen'>
+      <div className='flex flex-col justify-center items-center h-3/6'>
+        <p className='mb-2'>{todayDate}</p>
+        <div className='rounded-full border-4 border-solid border-white w-32 h-32 flex justify-center items-center'>
+          <div className='flex items-center gap-2'>
+            <span className='text-5xl'>{todayPuchits.length}</span>
+            {goal > 0 && <span className='text-xl'>/ {goal}</span>}
+          </div>
+        </div>
+        <button
+          className='bg-white text-violet-700 px-6 py-2 mt-4 rounded'
+          onClick={() => addCigarette()}
+        >
+          ADD
+        </button>
+      </div>
+      {todayPuchits.length > 0 && <CigarettesList cigarettes={todayPuchits} />}
+    </div>
+  );
+};
+
+const CigarettesList = ({ cigarettes }) => {
+  const [activePuchitId, setActivePuchitId] = useState(null);
+
+  return (
+    <div className='h-3/6 overflow-y-auto p-4'>
+      <ul className='flex flex-col gap-2'>
+        {cigarettes.map(({ id, date }) => (
+          <CigaretteItem
+            id={id}
+            key={id}
+            date={date}
+            activePuchitId={activePuchitId}
+            onSetActivePuchit={setActivePuchitId}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const CigaretteItem = ({ id, date, activePuchitId, onSetActivePuchit }) => {
+  const { removeCigarette } = useContext(MainContext);
+
+  const handleRemoveCigarette = (event) => {
+    event.stopPropagation();
+    removeCigarette(id);
+  };
+
+  return (
+    <li
+      onClick={() =>
+        activePuchitId !== id ? onSetActivePuchit(id) : onSetActivePuchit(null)
+      }
+      key={id}
+      className={cn(
+        'pl-4',
+        'pr-2',
+        'h-8',
+        'transition-colors',
+        'flex',
+        'justify-between',
+        'items-center',
+        {
+          'bg-violet-900': activePuchitId !== id,
+          'bg-white': activePuchitId === id,
+          'text-violet-900': activePuchitId === id
+        }
+      )}
+    >
+      <p>{getTimeString(date)}</p>
+      {activePuchitId === id && (
+        <div className='flex'>
+          <button
+            className='h-8 w-8 text-center leading-8'
+            onClick={handleRemoveCigarette}
+          >
+            <Icon name='trash-alt' />
+          </button>
+          <div
+            className='h-8 w-8 text-center leading-8'
+            onClick={() => onSetActivePuchit(null)}
+          >
+            <Icon name='close' />
+          </div>
+        </div>
+      )}
+    </li>
+  );
+};
+
+export default Home;
