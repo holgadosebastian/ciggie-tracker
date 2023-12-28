bb452ac1d926567d6d978ee10f2b0bd197a7efe0
Sebastian Holgado <holgado.sebastian@gmail.com>
2023-12-28 13:51:24 +0100

    Add Container component to Header

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export { Icon } from './Icon';
 export { Surface } from './Surface';
 export { Button } from './Button';
 export { Text } from './Text';
+export { Container } from './Container';
 export { Input, Label, FormField } from './FormField';
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -2,31 +2,30 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import cn from 'classnames';
 
-import { Surface } from '../components';
+import { Surface, Container } from '../components';
 
 const Header = () => {
   const { pathname } = useLocation();
 
   return (
     <>
-      <Surface
-        as='header'
-        className='fixed inset-x-0 top-0 py-2 px-4 h-10 flex gap-x-3 justify-between'
-      >
-        <nav className='flex gap-4'>
-          <Link
-            to='/'
-            className={cn('uppercase', { underline: pathname === '/' })}
-          >
-            Home
-          </Link>
-          {/* <Link
+      <Surface as='header' className='fixed inset-x-0 top-0 py-2 h-10'>
+        <Container>
+          <nav className='flex gap-4'>
+            <Link
+              to='/'
+              className={cn('uppercase', { underline: pathname === '/' })}
+            >
+              Home
+            </Link>
+            {/* <Link
             to='/history'
             className={cn('uppercase', { underline: pathname === '/history' })}
           >
             History
           </Link> */}
-        </nav>
+          </nav>
+        </Container>
       </Surface>
     </>
   );
