6a61b4276606e14650d8d88ffaa034bdc92a3484
Sebastian Holgado <holgado.sebastian@gmail.com>
2023-12-28 14:45:29 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx,html}",
-    "./public/index.html"
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx,html}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lighter: 'var(--color-lighter)',
+        light: 'var(--color-light)',
+        dark: 'var(--color-dark)',
+        darker: 'var(--color-darker)'
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
