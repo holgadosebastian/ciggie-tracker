6f6d62b247d741416336c31321e975bcfef784a0
Sebastian Holgado <holgado.sebastian@gmail.com>
2023-12-29 09:50:33 +0100

    Fix Tailwind custom CSS not loading in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import '!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css';
+import '!style-loader!css-loader!postcss-loader!../src/styles/index.css';
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
