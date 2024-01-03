7404a316b6d8019bb8d77d1210b988da5a728fb1
Sebastian Holgado <holgado.sebastian@gmail.com>
2024-01-03 13:04:20 +0100

    Add New Habit button in Dashboard page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,43 +2,78 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import MainContext from '../context/MainContext';
-import { Container, Surface, Text, Icon } from '../components';
+import {
+  Container,
+  Surface,
+  Text,
+  Icon,
+  FormField,
+  Label,
+  Button
+} from '../components';
 import { getStoredTabInfo } from '../lib/utils';
+import { COLORS, THEME_ICONS } from '../lib/const';
 
 const Dashboard = () => {
   const { tabs } = useContext(MainContext);
   const [items, setItems] = useState([]);
+  const [newHabitOpen, setNewHabitOpen] = useState(false);
 
   useEffect(() => {
-    setItems(() => {
-      return tabs.map((tab) => {
-        return getStoredTabInfo(tab.id);
-      });
-    });
-  }, []);
+    setItems(tabs.map((tab) => getStoredTabInfo(tab.id)));
+  }, [tabs]);
 
   return (
-    <div className='h-[calc(100vh-6.5rem)]'>
-      <Container className='h-full flex flex-col gap-4'>
-        <Text size='h4' className='uppercase'>
-          Your habits
-        </Text>
-        {items.map(({ id, name, themeColor, icon, occurrences }) => (
-          <TabItem
-            key={id}
-            id={id}
-            name={name}
-            themeColor={themeColor}
-            icon={icon}
-            occurrences={occurrences}
-          />
-        ))}
-      </Container>
-    </div>
+    <>
+      <div className='h-[calc(100vh-6.5rem)]'>
+        <Container className='h-full flex flex-col gap-4'>
+          <Text size='h4' className='uppercase'>
+            Your habits
+          </Text>
+
+          {items.map(({ id, name, themeColor, icon, occurrences }) => (
+            <HabitItem
+              key={id}
+              id={id}
+              name={name}
+              themeColor={themeColor}
+              icon={icon}
+              occurrences={occurrences}
+            />
+          ))}
+
+          <Surface
+            color='white'
+            padding='md'
+            rounded='default'
+            outline
+            onClick={() => setNewHabitOpen(true)}
+          >
+            <div className='flex gap-5 items-center'>
+              <Surface
+                className='flex items-center justify-center shrink-0 w-10 h-10'
+                rounded='default'
+              >
+                <Icon name='plus' color='mono-dark' />
+              </Surface>
+              <div>
+                <Text size='h5' className='uppercase'>
+                  New Habit
+                </Text>
+              </div>
+            </div>
+          </Surface>
+        </Container>
+      </div>
+      <NewHabitModal
+        isOpen={newHabitOpen}
+        onClose={() => setNewHabitOpen(false)}
+      />
+    </>
   );
 };
 
-const TabItem = ({ id, name, themeColor, icon, occurrences }) => {
+const HabitItem = ({ id, name, themeColor, icon, occurrences }) => {
   const navigate = useNavigate();
   const { setCurrentTab } = useContext(MainContext);
 
@@ -69,6 +104,84 @@ const TabItem = ({ id, name, themeColor, icon, occurrences }) => {
           <Text size='small'>Today: {occurrences.length}</Text>
         </div>
       </div>
+    </Surface>
+  );
+};
+
+const NewHabitModal = ({ isOpen, onClose }) => {
+  const { addTab } = useContext(MainContext);
+  const [name, setName] = useState('Item 1');
+  const [themeColor, setThemeColor] = useState('violet');
+  const [icon, setIcon] = useState('fire');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    addTab({ name, themeColor, icon });
+    onClose();
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <Surface
+      className='fixed top-0 left-0 w-full h-full'
+      padding='md'
+      color='mono-dark'
+    >
+      <button
+        className='absolute top-4 right-4'
+        type='button'
+        onClick={() => onClose()}
+      >
+        <Icon name='close' />
+      </button>
+
+      <p className='text-xl mb-5'>Add a New Tab</p>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <FormField
+          label='Name'
+          name='name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <FormField
+          label='Color'
+          name='themeColor'
+          type='select'
+          value={themeColor}
+          onChange={(e) => setThemeColor(e.target.value)}
+        >
+          {Object.values(COLORS).map((value) => (
+            <option key={value} value={value}>
+              {value}
+            </option>
+          ))}
+        </FormField>
+        <div className='flex flex-col gap-2'>
+          <Label>Icon</Label>
+          <div className='grid grid-cols-5 gap-4'>
+            {Object.values(THEME_ICONS).map((value) => (
+              <Surface
+                key={value}
+                className='flex items-center justify-center'
+                padding='sm'
+                color='white'
+                rounded='default'
+                outline={value !== icon}
+                onClick={() => setIcon(value)}
+              >
+                <Icon
+                  name={value}
+                  size='xl'
+                  color={value !== icon ? 'white' : 'mono-dark'}
+                />
+              </Surface>
+            ))}
+          </div>
+        </div>
+        <Button type='submit'>ADD</Button>
+      </form>
     </Surface>
   );
 };
--- src/pages/Habit/Tabs.js
+++ src/pages/Habit/Tabs.js
@@ -1,19 +1,10 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 
-import {
-  Icon,
-  Surface,
-  Button,
-  FormField,
-  Text,
-  Label
-} from '../../components';
-import { COLORS, THEME_ICONS } from '../../lib/const';
+import { Icon, Surface, Text } from '../../components';
 import MainContext from '../../context/MainContext';
 
 export const Tabs = () => {
   const { currentTab, setCurrentTab, tabs } = useContext(MainContext);
-  const [newTabOpen, setNewTabOpen] = useState(false);
 
   return (
     <div className='h-12 flex fixed bottom-0 left-0 w-full border-t-dark border-t-2'>
@@ -29,91 +20,6 @@ export const Tabs = () => {
           <Text size='tiny'>{name}</Text>
         </Surface>
       ))}
-      <button
-        className='uppercase h-12 w-12 px-4 leading-12 text-darker ml-auto bg-slate-700'
-        onClick={() => setNewTabOpen(true)}
-      >
-        <Icon name='plus' />
-      </button>
-      <NewTab isOpen={newTabOpen} onClose={() => setNewTabOpen(false)} />
     </div>
-  );
-};
-
-const NewTab = ({ isOpen, onClose }) => {
-  const { addTab } = useContext(MainContext);
-  const [name, setName] = useState('Item 1');
-  const [themeColor, setThemeColor] = useState('violet');
-  const [icon, setIcon] = useState('fire');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    addTab({ name, themeColor, icon });
-    onClose();
-  };
-
-  if (!isOpen) return null;
-
-  return (
-    <Surface
-      className='absolute bottom-0 w-full'
-      padding='md'
-      color='mono-dark'
-    >
-      <button
-        className='absolute top-4 right-4'
-        type='button'
-        onClick={() => onClose()}
-      >
-        <Icon name='close' />
-      </button>
-
-      <p className='text-xl mb-5'>Add a New Tab</p>
-      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-        <FormField
-          label='Name'
-          name='name'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <FormField
-          label='Color'
-          name='themeColor'
-          type='select'
-          value={themeColor}
-          onChange={(e) => setThemeColor(e.target.value)}
-        >
-          {Object.values(COLORS).map((value) => (
-            <option key={value} value={value}>
-              {value}
-            </option>
-          ))}
-        </FormField>
-        <div className='flex flex-col gap-2'>
-          <Label>Icon</Label>
-          <div className='grid grid-cols-5 gap-4'>
-            {Object.values(THEME_ICONS).map((value) => (
-              <Surface
-                key={value}
-                className='flex items-center justify-center'
-                padding='sm'
-                color='white'
-                rounded='default'
-                outline={value !== icon}
-                onClick={() => setIcon(value)}
-              >
-                <Icon
-                  name={value}
-                  size='xl'
-                  color={value !== icon ? 'white' : 'mono-dark'}
-                />
-              </Surface>
-            ))}
-          </div>
-        </div>
-        <Button type='submit'>ADD</Button>
-      </form>
-    </Surface>
   );
 };
