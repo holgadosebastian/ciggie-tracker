474227ce796cc3cecefcfad8c8a22a55b9fd84d3
Sebastian Holgado <holgado.sebastian@gmail.com>
2024-01-04 14:32:10 +0100

    Add centering to Container component

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,7 +3,7 @@ import cn from 'classnames';
 
 export const Container = ({ children, className, ...props }) => {
   return (
-    <div className={cn('px-4 max-w-sm', className)} {...props}>
+    <div className={cn('mx-auto px-4 max-w-sm', className)} {...props}>
       {children}
     </div>
   );
